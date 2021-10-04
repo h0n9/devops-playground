deb5eef4ec6f9f15574d1996c2de5dd21aa2fa67
Jaehong Ahn <monsieur.ahn@gmail.com>
2021-10-05 01:17:33 +0900

    Fix wrongly implement NAT, route table

--- terraform/modules/vpc-cheap/main.tf
+++ terraform/modules/vpc-cheap/main.tf
@@ -69,6 +69,19 @@ resource "aws_nat_gateway" "this" {
   })
 }
 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = merge(var.tags, {
+    "Name" = format("%s-%s", var.name, "route-table-public")
+  })
+}
+
 resource "aws_route_table" "private" {
   count = length(var.aws_azs)
 
@@ -84,9 +97,33 @@ resource "aws_route_table" "private" {
   })
 }
 
+resource "aws_route_table_association" "public" {
+  count = length(var.aws_public_subnets)
+
+  route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.public.*.id[count.index]
+}
+
 resource "aws_route_table_association" "private" {
   count = length(var.aws_azs)
 
   route_table_id = aws_route_table.private.*.id[count.index]
   subnet_id      = aws_subnet.private.*.id[count.index]
+}
+
+resource "aws_security_group" "to-out" {
+  name   = "to-out"
+  vpc_id = aws_vpc.this.id
+
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Name"              = "h0n9-security-group-to-out"
+    "terraform-managed" = "true"
+  }
 }
